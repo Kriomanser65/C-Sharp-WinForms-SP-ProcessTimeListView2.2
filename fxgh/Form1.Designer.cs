39c49c985e8f95e9ba8435df81f3a71b94359b09
Stekoks <Stekoks@DESKTOP-4SK39GD>
2023-11-28 15:32:44 +0200

    Add project files.

---
+++ fxgh/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace fxgh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(716, 323);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(12, 341);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(605, 97);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(734, 13);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(624, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 96);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "END PROCESS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ fxgh/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace fxgh
+{
+    public partial class Form1 : Form
+    {
+        private Timer updateTimer;
+        private int refreshInterval = 1000;
+        private Process[] processes;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimer();
+            LoadProcesses();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                Process selectedProcess = processes[listView1.SelectedIndices[0]];
+                DisplayProcessDetails(selectedProcess);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            refreshInterval = (int)numericUpDown1.Value;
+            updateTimer.Interval = refreshInterval;
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                Process selectedProcess = processes[listView1.SelectedIndices[0]];
+                EndSelectedProcess(selectedProcess);
+            }
+        }
+        private void EndSelectedProcess(Process process)
+        {
+            try
+            {
+                process.Kill();
+                MessageBox.Show($"Process {process.ProcessName} ended successful.");
+                LoadProcesses();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+        private void InitializeTimer()
+        {
+            updateTimer = new Timer();
+            updateTimer.Interval = refreshInterval;
+            updateTimer.Tick += UpdateProcessList;
+            updateTimer.Start();
+        }
+        private void UpdateProcessList(object sender, EventArgs e)
+        {
+            LoadProcesses();
+        }
+        private void LoadProcesses()
+        {
+            processes = Process.GetProcesses();
+            listView1.Items.Clear();
+            foreach (Process process in processes)
+            {
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.SubItems.Add(process.Id.ToString());
+                item.SubItems.Add(process.Responding ? "Yes" : "No");
+                listView1.Items.Add(item);
+            }
+        }
+        private void DisplayProcessDetails(Process process)
+        {
+            listView2.Items.Clear();
+            ListViewItem idItem = new ListViewItem("Process ID");
+            idItem.SubItems.Add(process.Id.ToString());
+            listView2.Items.Add(idItem);
+            ListViewItem startTimeItem = new ListViewItem("Start Time");
+            startTimeItem.SubItems.Add(process.StartTime.ToString());
+            listView2.Items.Add(startTimeItem);
+            ListViewItem cpuTimeItem = new ListViewItem("CPU Time");
+            cpuTimeItem.SubItems.Add(process.TotalProcessorTime.ToString());
+            listView2.Items.Add(cpuTimeItem);
+            ListViewItem threadCountItem = new ListViewItem("Thread Count");
+            threadCountItem.SubItems.Add(process.Threads.Count.ToString());
+            listView2.Items.Add(threadCountItem);
+        }
+    }
+}
